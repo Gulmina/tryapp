d8dd97819bd04a544a4262318535935f719e0324
gulmina <gulmina44@gmail.com>
2025-09-22 13:21:45 +0200

    my first page is ready to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import React from 'react';
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-       <h1 className="text-3xl ">MY CV world</h1>
+    
+      <div className="h-screen flex items-center justify-center">
+       <h1 className="text-center text-3xl font-bold ">MY CV world</h1>
       </div>
      
      
-    </>
+  
   )
 }
 
